29fe80127eeb8a5bcec80f9f434ba83f7e22202f
Jean Paulo <contatos.jean@gmail.com>
2022-02-09 11:07:54 -0300

    Function que não existe com sucesso no monitoramento

--- mock.go
+++ mock.go
@@ -11,7 +11,7 @@ type MockFunctions struct {
 	mock.Mock
 }
 
-func (mock MockFunctions) GetLastFunctionsRun(name, validationString, locations, string, seconds time.Duration) (lastRun *domain.FunctionsLastRun, err error) {
+func (mock MockFunctions) GetLastFunctionsRun(name, validationString, locations string, seconds time.Duration) (lastRun *domain.FunctionsLastRun, err error) {
 	args := mock.Called(name, validationString, locations, seconds)
 	return args.Get(0).(*domain.FunctionsLastRun), args.Error(1)
 }
